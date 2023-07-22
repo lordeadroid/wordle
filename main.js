b75bdc18de862d8d643498796bb69ca37f9a5f34
Rishabh <lordeadroid@users.noreply.github.com>
2023-07-22 16:20:43 +0530

    can also indicate about wrong guess

--- main.js
+++ main.js
@@ -2,20 +2,28 @@ const checkAnswer = (userAnswer) => {
   return userAnswer === "hello";
 };
 
+const displayResult = (message) => {
+  const page = document.querySelector("#page");
+  const resultBox = document.createElement("div");
+
+  resultBox.innerText = message;
+  page.appendChild(resultBox);
+};
+
 const main = () => {
   const answerBox = document.querySelector("#answer-box");
   const submitButton = document.querySelector("#submit-button");
 
   submitButton.onclick = () => {
     const userAnswer = answerBox.value;
+    answerBox.value = "";
     const result = checkAnswer(userAnswer);
 
     if (result) {
-      const page = document.querySelector("#page");
-      const answerElement = document.createElement("div");
-      answerElement.innerText = "Your Guess Is Correct";
-      page.appendChild(answerElement);
+      displayResult("Your answer was Correct");
+      return;
     }
+    displayResult("Your Answer Was Incorrect");
   };
 };
 
