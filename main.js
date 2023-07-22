03e14786fad69567e60dc54f2db2ce94b2660e7d
Rishabh <lordeadroid@users.noreply.github.com>
2023-07-22 16:09:12 +0530

    indicate if guess is correct

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-window.onload = () => {};
+const checkAnswer = (userAnswer) => {
+  return userAnswer === "hello";
+};
+
+const main = () => {
+  const answerBox = document.querySelector("#answer-box");
+  const submitButton = document.querySelector("#submit-button");
+
+  submitButton.onclick = () => {
+    const userAnswer = answerBox.value;
+    const result = checkAnswer(userAnswer);
+
+    if (result) {
+      const page = document.querySelector("#page");
+      const answerElement = document.createElement("div");
+      answerElement.innerText = "Your Guess Is Correct";
+      page.appendChild(answerElement);
+    }
+  };
+};
+
+window.onload = main;
